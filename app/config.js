1e1126dd3279986953a5bc9e68f526f2667bf1b8
edgardlobo <edgard.alobo@gmail.com>
2017-12-07 17:19:49 -0200

    add config

---
+++ app/config.js
@@ -0,0 +1,25 @@
+/** Config */
+
+module.exports = {
+	//DEFAULT_API_VERSION: '20160516',
+    DEFAULT_API_VERSION: process.env.DEFAULT_API_VERSION,
+
+    DEFAULT_MAX_STEPS: 5,
+    DEFAULT_WIT_URL: 'https://api.wit.ai',
+
+	//application devBotTest2
+	FB_PAGE_TOKEN: 'EAAJ4KZBf2Y4oBABetU8XFez0hd3O6sZBzamFmGoeSdZBfdVbQXDNT47W9oZAn3Hoi28kEBL1PQvQFr4TF5wRXIRZBFeYOOLMQHzFznp4J2Txb2af2upIpUY3iklKAmjlEbVbifhaZABNycuFGMSLZAcXe00bN8SUgJWlqhePPE7TAZDZD',
+    //FB_PAGE_TOKEN: process.env.FB_PAGE_TOKEN,
+
+    //id de identificação da página DONM
+    FB_PAGE_ID: '165912047280389',
+    //FB_PAGE_ID: process.env.FB_PAGE_ID,
+
+    //senha do aplicatido DONM (devBotTest2)
+    FB_VERIFY_TOKEN: 'minhasenhafb123',
+    //FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
+
+    //token da aplicação no api.ai
+    API_AI_TOKEN: '9d6819961fae4288bd3097c4ae2a2770',
+    //API_AI_TOKEN: process.env.API_AI_TOKEN,
+};
